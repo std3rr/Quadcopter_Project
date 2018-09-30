59a7f1fde56188ad39e848b423780d3d8b6dc2ec
Andreas Lewitzki <40644185+std3rr@users.noreply.github.com>
2018-09-30 13:16:26 +0200

    Add files via upload

--- task.py
+++ task.py
@@ -1,9 +1,10 @@
 import numpy as np
+import math
 #from physics_sim import PhysicsSim
 
 class Hover():
     """Task (environment) that defines the goal and provides feedback to the agent."""
-    def __init__(self, init_pose=None, init_velocities=None, 
+    def __init__(self, init_pose=None, init_velocities=None,
         init_angle_velocities=None, runtime=5., target_pos=None):
         """Initialize a Task object.
         Params
@@ -15,54 +16,60 @@ class Hover():
             target_pos: target/goal (x,y,z) position for the agent
         """
         # Simulation
-        self.sim = PhysicsSim(init_pose, init_velocities, init_angle_velocities, runtime) 
+        self.sim = PhysicsSim(init_pose, init_velocities, init_angle_velocities, runtime)
         self.action_repeat = 3
 
         self.state_size = self.action_repeat * (len(self.sim.pose))
-        self.action_low = 0
+        self.action_low = 600
         self.action_high = 900
         self.action_size = 4
         self.prev_distance = None
 
         # Goal
-        self.target_pos = target_pos if target_pos is not None else np.array([0., 0., 10.]) 
+        self.target_pos = target_pos if target_pos is not None else np.array([0., 0., 10.])
 
     def get_reward(self):
         """Reward function. Been exploring alot here."""
-        #print("POSITION:\n  {:3.4f},{:3.4f},{:3.4f}".format(self.sim.pose[0],self.sim.pose[1],self.sim.pose[2]))
-        #sum_distance = (abs(self.sim.pose[:3] - self.target_pos)).sum()
+
         # D = √[(x₂ - x₁)² + (y₂ - y₁)² +(z₂ - z₁)²]
         distance = np.linalg.norm(self.sim.pose[:3] - self.target_pos)
-        #x0,y0,z0 = self.target_pos[:3]
-        #x1,y2,z1 = self.sim.pose[:3] 
-    
-        #reward = 1.- .003*sum_distance
-        #if sum_distance < 0.4:
-        #    return 100
-        #reward = (10.+self.sim.time) - ( distance * (abs(self.sim.v)).sum() )
-        #max_xy_angle = max(abs(self.sim.pose[3]),abs(self.sim.pose[4]))
-        
-        # we want to "stay" ..so we use every signal that we are not staying as sum penalty
-        #sum_distance = (abs(self.sim.pose[:3] - self.target_pos)).sum()
-        #sum_angle = abs(self.sim.pose[3:]).sum()
-        #sum_velocity = abs(self.sim.v).sum()
-        #penalty = (sum_distance+(sum_velocity*0.01)) # + sum_velocity + sum_angle)
-        #reward = (1+self.sim.time) - (distance + (angle / 4))
-        #reward = 25 - penalty
+        velocity = np.linalg.norm(self.sim.v+0.0)
 
-        #reward = 10 - distance # + (sum_angle*0.025)
-        #reward = reward * 0.1
-        
-        #punishment = -( distance + sum_angle + sum_velocity)
-        
-        reward  = 100. - (distance * 0.1)
+        #distance = abs(sum(self.sim.pose[:3] - self.target_pos))
+        #velocity = abs(sum(self.sim.v))
+        #if(distance == nan):
+
+        reward  = 100. - (distance * velocity)
+        #reward  = 100. - distance
+        if reward <= 0.0:
+            reward = 0.0
+        return reward
+        if math.isnan(distance) or math.isnan(velocity):
+            return 0.0
+
+        print(distance, velocity)
+        #distance = distance * 0.003
+        dist_reward = 1. - (distance**0.4)
+        #print("drew:",dist_reward)
+        #print(np.max([velocity,0.1]))
+        avel = (1. - max([velocity,0.1]))
+        adist = (1./max([distance,0.1]))
+        #print("avel:",avel)
+        #print("adist:",adist)
+        vel_discount = avel ** adist
+        #print("vdis:",vel_discount)
+        reward = np.max([vel_discount * dist_reward,0.0])
+        #print("rew:",reward)
+
+
+        #reward  = 100. - (distance * 0.1)
         #if self.prev_distance == None or self.prev_distance == distance:
-        #    reward = 10.0 - (distance * 0.1)
+    #        reward = 10.0 - (distance * 0.1)
         #elif self.prev_distance > distance:
         #    reward = 20.0 - (distance * 0.1)
         #elif self.prev_distance < distance:
         #    reward = -20.0 - (distance * 0.1)
-            
+
         self.prev_distance = distance
         return reward
 
@@ -93,8 +100,8 @@ class Hover():
         """Reset the sim to start a new episode."""
         self.sim.reset()
         distances = np.array(self.sim.pose[:3] - self.target_pos)
-        state = np.concatenate([self.sim.pose]) 
-        state = np.concatenate([state] * self.action_repeat) 
+        state = np.concatenate([self.sim.pose])
+        state = np.concatenate([state] * self.action_repeat)
         return state
 
 
@@ -107,8 +114,8 @@ class Task():
        The task to be learned is how to takeoff.
        The quadcopter has an initial position of (0, 0, 0) and a target position of (0, 0, 20)
     """
-    
-    def __init__(self, init_pose=None, init_velocities=None, 
+
+    def __init__(self, init_pose=None, init_velocities=None,
         init_angle_velocities=None, runtime=5., target_pos=None):
         """Initialize a Task object.
         Params
@@ -120,7 +127,7 @@ class Task():
             target_pos: target/goal (x,y,z) position for the agent
         """
         # Simulation
-        self.sim = PhysicsSim(init_pose, init_velocities, init_angle_velocities, runtime) 
+        self.sim = PhysicsSim(init_pose, init_velocities, init_angle_velocities, runtime)
         self.action_repeat = 3
 
         self.state_size = self.action_repeat * 6
@@ -129,13 +136,13 @@ class Task():
         self.action_size = 4
 
         # Goal
-        self.target_pos = target_pos if target_pos is not None else np.array([0., 0., 10.]) 
+        self.target_pos = target_pos if target_pos is not None else np.array([0., 0., 10.])
 
     def get_reward(self):
         """Uses current pose of sim to return reward."""
         # this is the original, commented out
         # reward = 1.-.3*(abs(self.sim.pose[:3] - self.target_pos)).sum()
-        
+
         # new reward function, start reward at 0
         reward = 0.
         # calculate the coordinate distance from the target position
@@ -149,8 +156,8 @@ class Task():
         # calculate reward
         reward = reward + bonus - penalty
         return reward
-       
-        
+
+
 
     def step(self, rotor_speeds):
         """Uses action to obtain next state, reward, done."""
@@ -158,7 +165,7 @@ class Task():
         pose_all = []
         for _ in range(self.action_repeat):
             done = self.sim.next_timestep(rotor_speeds) # update the sim pose and velocities
-            reward += self.get_reward() 
+            reward += self.get_reward()
             pose_all.append(self.sim.pose)
         next_state = np.concatenate(pose_all)
         return next_state, reward, done
@@ -166,5 +173,5 @@ class Task():
     def reset(self):
         """Reset the sim to start a new episode."""
         self.sim.reset()
-        state = np.concatenate([self.sim.pose] * self.action_repeat) 
+        state = np.concatenate([self.sim.pose] * self.action_repeat)
         return state
